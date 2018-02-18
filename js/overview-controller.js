a8d216786580d379ec3653b3728a8ab1c97574a7
Jonathan <jpat82792@gmail.com>
2018-02-18 05:29:40 -0600

    add TODOs about the upcoming year view release

--- js/overview-controller.js
+++ js/overview-controller.js
@@ -1,5 +1,6 @@
 console.log('overview-controller.js');
-
+//TODO this was only used on the year view
+//TODO add a check to determine if in month or year
 var scrollToMonth = function(){
 /*  let calendarOverview = document.getElementById('calendar-overview-widget');
   let currentMonth = calendarOverview.querySelector('[data-month-current="true"]');
